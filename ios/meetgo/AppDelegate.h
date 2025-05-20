6bf2929c3270d3e1caf6e9f7f0bd0e3fc76b7166
unknown <thenguyenfiner@gmail.com>
2025-05-20 13:56:31 +0700

    Latest push

--- ios/meetgo/AppDelegate.h
+++ ios/meetgo/AppDelegate.h
@@ -1,4 +1,13 @@
+/**
+ * Copyright (c) mrousavy
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 #import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
-@interface AppDelegate : EXAppDelegateWrapper
+
+@interface AppDelegate : RCTAppDelegate
+
 @end
--- ios/meetgo/AppDelegate.mm
+++ ios/meetgo/AppDelegate.mm
@@ -1,6 +1,6 @@
 #import "AppDelegate.h"
-#import <React/RCTBundleURLProvider.h>
 #import <CodePush/CodePush.h>
+#import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
 
@@ -15,11 +15,11 @@
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
-  #if DEBUG
-    return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
-  #else
-    return [CodePush bundleURL];
-  #endif
+#if DEBUG
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+#else
+  return [CodePush bundleURL];
+#endif
 }
 
 @end
